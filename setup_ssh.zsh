c42196a22f76ec1c8a7ff3193367355de9d5a780
Nick Moore <nick@pilotmoon.com>
2022-12-22 13:46:04 +0000

    SSH config changes for github

--- setup_ssh.zsh
+++ setup_ssh.zsh
@@ -3,8 +3,11 @@ set -e
 echo "\n<<< Starting SSH Setup >>>\n"
 
 # use 1Passsword CLI to generaate the actual file (which is in .gitignore)
-echo "Generating SSH config file..."
+echo "Generating SSH config files..."
 op inject -f -i ssh/config_template -o ssh/config
+op inject -f -i ssh/github_template.pub -o ssh/github.pub
+op inject -f -i ssh/github1_template.pub -o ssh/github1.pub
+op inject -f -i ssh/allowed_signers_template -o ssh/allowed_signers
 
 # update this repo origin now that we can use SSH
 echo "Updating dotfiles repo origin URL..."
